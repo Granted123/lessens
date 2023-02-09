887d844378963ca4fff840714763ff71026ad31a
Granted123 <gosha.absandze@yandex.ru>
2023-02-10 01:27:37 +0300

    Home_work_module_6.5

--- Task_8.py
+++ Task_8.py
@@ -1,12 +1,11 @@
 desired_number = int(input("Загадайте число от 1 до 100: "))
-x = 50
-y = 0
-while True:
-    number = print("Ваше число,", x, "? ")
-    if number > y:
-
-    elif number < y:
-        print("Число меньше, чем нужно. Попробуйте ещё раз!")
-    else:
-        print("Вы угадали!!!")
-        break
+x = int(input("Ваше число: "))
+count = 1
+while x != desired_number:
+    if x > desired_number:
+        x = int(x - x/2)
+    elif x < desired_number:
+        x = int(x / 2)
+    count += 1
+print(count)
+print("Вы угадали!!!", x)
