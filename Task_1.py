205ee441fa02e3216a3b9274f4826dd9f5307c36
Granted123 <gosha.absandze@yandex.ru>
2023-03-27 16:23:25 +0300

    Home_work_module_9

--- Task_1.py
+++
@@ -1,7 +0,0 @@
-amount_of_numbers = int(input("До какого числа возводить в степень? "))
-count_number = 1
-while count_number <= amount_of_numbers:
- cube_number = count_number ** 3
- print(count_number,"в третьей степени =", cube_number)
- count_number +=1
-
